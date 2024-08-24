b12eb2b2338353ca7170583c4117cc4a5c666dde
not-rajankit <work.rajankit@gmail.com>
2024-08-24 18:58:34 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:8000/api/v1";
-export const BASE_URL = "https://bookify-backend-flame.vercel.app/api/v1";
+// export const BASE_URL = "https://bookify-backend-flame.vercel.app/api/v1";
+export const BASE_URL = "https://bookify-backend-gxsv.onrender.com/api/v1";
